622736a2aa508d5da8c7ddc1aa5469f934edada3
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-03-22 18:19:16 -0300

    Adicionei o Projeto completo

---
+++ Projeto/js/confirmaçaõ.js
@@ -0,0 +1,14 @@
+var name="Empresa XY";
+    alert("Matrícula efetuada com sucesso ");
+    
+var retorno= confirm("Deseja que seu boleto seja enviado via SMS ou Email?");
+
+var nomeUsuario = prompt("Qual o seu nome?", "Nome");
+if(nomeUsuario !="") {
+    document.write("Seja Bem-vindo ao Águia Thai "+nomeUsuario+"!");
+} else {
+    document.write("Poxa lutador, não sei nome");
+}
+
+
+    
